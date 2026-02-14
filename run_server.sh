2b7d3c1405d5af733b3d78eb6964310ccb7bf117
atul <atulg0736@gmail.com>
2026-02-15 02:51:06 +0530

    charts

--- run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting TradeHub server with WebSocket support..."
-echo ""
-echo "Make sure you have activated your virtual environment!"
-echo ""
-daphne -b 0.0.0.0 -p 8000 tradehub.asgi:application
-
